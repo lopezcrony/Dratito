d7b287252a47c08416b9371ad82b9d3ba5fdfa56
lopezcrony <lopezcrony@gmail.com>
2024-04-30 00:36:54 -0500

    Add project files.

---
+++ Clases/Modulos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    internal class Modulos
+    {
+        Console.WriteLine();
+
+    }
+}
+
+
+public class devoluciones
+{
+    
+
+} 
---
+++ Clases/Program.cs
@@ -0,0 +1,94 @@
+﻿using System.Drawing;
+
+public class Clases
+{
+
+    public static void Main()
+    {
+        Puerta puerta1 = new Puerta();
+        puerta1.abrir();
+        puerta1.MostrarEstado();
+
+        Porton porton1 = new Porton();
+        porton1.bloquear();
+        porton1.MostrarEstado();
+        porton1.desbloquear();
+        porton1.MostrarEstado();
+
+
+
+        Puerta[] arrayPuerta = new Puerta[5];
+    }
+}
+
+public class Puerta{
+    protected int ancho { set; get; } /*Proteger atributos a nivel de herencia*/
+    protected String? color { set; get; }
+    protected int alto { set; get; }
+    protected bool abierto { set; get; }
+
+    //constructor Vacio (es por defecto, pero acá todo)
+    public Puerta() { }
+
+    //constructor
+    public Puerta(int ancho, int alto, string color, bool abierto)
+    {
+        this.ancho = ancho;
+        this.alto = alto;
+        this.color = color;
+        this.abierto = abierto;
+    }
+
+
+    ~Puerta(){//Destruir contructor
+
+        //Liberra memoria
+        //Cerrar ficheros
+    }
+
+
+        public bool abrir() {
+        return abierto = true;
+    }
+
+    public bool cerrar()
+    {
+        return abierto = false;  
+    }
+
+    public void MostrarEstado() {
+        Console.WriteLine("Ancho: {0}", ancho);
+        Console.WriteLine("Alto: {0}", alto);
+        Console.WriteLine("Color: {0}", color);
+        Console.WriteLine("Estado: {0}", abierto);
+
+    }
+
+}
+
+public class Porton:Puerta{ /*La herencia se hace por medio de 2 puntos*/
+    bool bloqueada;
+
+    public bool bloquear()
+    {
+        return bloqueada = true;
+    }
+
+    public bool desbloquear()
+    {
+        return bloqueada=false;
+    }
+
+
+    public new void MostrarEstado()
+    {
+        Console.WriteLine("Ancho: {0}", ancho);
+        Console.WriteLine("Alto: {0}", alto);
+        Console.WriteLine("Color: {0}", color);
+        Console.WriteLine("Estado: {0}", abierto);
+        Console.WriteLine("Bloqueada: {0}", bloqueada);
+
+    }
+}
+
+
